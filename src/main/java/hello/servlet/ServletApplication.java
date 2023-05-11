dc5996cee3dfb4c958cdc3e427840dc984291b2f
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-05-11 13:59:12 +0900

    -springMVC 구조 아키텍처 연습 설명 : springMVC가 사용되기 전부터의 과정과 SpringMVC로 변환되는 과정을 공부하였음. 요점 : SpringMVC변환 과정의 핵심으로는 dispather-Servlet을 통해 여러 servlet을 관리 할 수 있고 또한 다른 controller의 mapping 요청을 spring에서 제공하는 RequestMappingHandlerMapping을 이용하여 @Controller를 통해 RequestMapping이 달려 있는 mapping 정보를 모두 올려놓고 handler 조회를 통해 RequestMappingHandlerAdapter를 통해 어뎁터 정보(요청을 수행할 수 있는 controller)  handle를 통해 해당 요청을 받을수있는 컨트롤러에서 프로세스 처리 후 Model 정보와 논리적 URL ex) "index" 를 반환 하여 viewResolver  인터페이스를 통해 구체화된 해당 사항에 맞는 viewResolver을 통해 view정보를 반환하고 model 객체의 정보를 사용자에게 다시 반환해 주는 과정을 더욱 정확하게 이해하게 되었음

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,13 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	//Bean등록으로도 controller실행 가능 다만 Componet만 등록되므로
+	//@RequestMapping을 controller에 달아줘야 RequestMappingHandlerMapping가 mapping정보로 인식하여 등록함
+
+	/*@Bean
+	SpringMemberFormControllerV1 SpringMemberFormControllerV1(){
+		return new SpringMemberFormControllerV1();
+	}*/
 
 }
